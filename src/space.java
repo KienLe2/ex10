ded09e6147f7864dab800069cc9cd69c19cf6142
student <student@vlab.local>
2023-12-14 09:20:12 +0100

    ex10

---
+++ src/space.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class space {
+    public static void main(String[] args){
+//        int spacetodash = 0;
+//
+//        try(
+//                FileInputStream input = new FileInputStream("studentEN.txt");
+//                FileOutputStream output = new FileOutputStream("student-output.txt")){
+//            do{
+//                spacetodash = input.read();
+//                if((char)spacetodash == ' ') spacetodash = '-';
+//                if(spacetodash != -1) output.write(spacetodash);
+//            }while(spacetodash != -1);
+//        } catch (IOException e){
+//            System.out.println("Error IO");
+//        }
+//        try(BufferedReader br = new BufferedReader(new FileReader("studentEN.txt"));
+//            FileWriter fw = new FileWriter("studentEN-5.txt")){
+//            String line;
+//            while((line = br.readLine()) != null){
+//                String[] words = line.split("\\s+");
+//                String surname = words[0].trim();
+//                String name = words[1].trim();
+//                String score = words[2].trim();
+//                if(score.equals("5")){
+//                    fw.write(surname + "\s" + name + "\s" + score + "\n");
+//                }
+//            }
+//        }catch(IOException e){
+//            System.out.println("Error IO");
+//        }
+    }
+}
